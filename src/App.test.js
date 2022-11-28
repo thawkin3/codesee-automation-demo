2328c0af916faf850381b83785f57c4d322bfce1
Tyler Hawkins <thawkin3@byu.net>
2022-11-28 11:11:47 -0700

    feat: initializes basic icon library structure

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders icon library header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText('Icon Library')).toBeInTheDocument();
 });
