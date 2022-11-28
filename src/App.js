c6c03130a05be732605a32a8e49498ceee69b4f8
Tyler Hawkins <thawkin3@byu.net>
2022-11-28 12:24:38 -0700

    feat: adds several icons and a showcase page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg'
+import * as icons from './icons'
 import './App.css'
 
 function App() {
   return (
     <main className="app">
       <h1>Icon Library</h1>
-      <div>TODO: Show about 8 icons here</div>
+      <div className="iconsContainer">
+        {Object.entries(icons).map(([key, Icon]) => (
+        <div className="iconContainer" key={key}>
+          <Icon />
+          <div>{key}</div>
+        </div>
+        ))}
+      </div>
+      <p className="footnote">Icons used with permission from <a href="https://www.svgrepo.com/collection/avatar-duotone-vectors/">svgrepo.com</a></p>
     </main>
   )
 }
